4b49326f78fd83e6da5c258f92cb5fe4c003f7e1
Sourabh Yadav <sourabhyadav@Sourabhs-MacBook-Air.local>
2025-12-16 13:13:56 +0530

    updated apis

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     plugins: [react()],
     server: {
-        host: '0.0.0.0',
+        host: 'https://tastelab-collab.onrender.com/',
         port: parseInt(process.env.PORT, 10) || 5173,
         proxy: {
             '/api': {
